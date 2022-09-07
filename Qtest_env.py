bf7fb2afcfaee3b763f5853e0bc225289dd6f31a
shaun <shaunlee8@gmail.com>
2022-09-06 21:26:57 -0700

    wrote a class for the game

--- Qtest_env.py
+++ Qtest_env.py
@@ -17,50 +17,107 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 DEVICE = 'cpu' # 'cuda' if torch.cuda.is_available() else 'cpu'
 
-pygame.init()
 
-vec = pygame.math.Vector2  # 2 for two dimensional
-FramePerSec = pygame.time.Clock()
-brick = pygame.image.load("background.png")
-displaysurface = pygame.display.set_mode((params.WIDTH, params.HEIGHT))
-pygame.display.set_caption("Game")
+class SpiderJumpGame:
+    def __init__(self):
+        pygame.display.set_caption("SpiderJump")
+        self.displaysurface = pygame.display.set_mode((params.WIDTH, params.HEIGHT))
+        self.spider = player.Player()
+        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites.add(self.spider)
+        self.plats = pygame.sprite.Group()
+        self.plat_image = pygame.image.load('wood_platform.png')
+        self.plat_image.set_colorkey((0, 0, 0))
+        self.background = pygame.image.load("background.png")
+        self.FramePerSec = pygame.time.Clock()
 
 
-def set_background():
+def set_background(displaysurface, background):
     displaysurface.fill(params.WHITE)
-    brick_width, brick_height = brick.get_width(), brick.get_height()
-    for x, y in itertools.product(range(0, params.WIDTH, brick_width), range(0, params.HEIGHT, brick_height)):
-        displaysurface.blit(brick, (x, y))
+    bg_width, bg_height = background.get_width(), background.get_height()
+    for x, y in itertools.product(range(0, params.WIDTH, bg_width), range(0, params.HEIGHT, bg_height)):
+        displaysurface.blit(background, (x, y))
 
 
-# Initiate player and starting platform
-spider = player.Player()
-plat_image = pygame.image.load('wood_platform.png')
-plat_image.set_colorkey((0,0,0))
-platform1 = platforms.Platform()
-platform1.surf = pygame.transform.scale(plat_image, (params.WIDTH, 20))
-platform1.rect = platform1.surf.get_rect(center=(params.WIDTH / 2, params.HEIGHT - 10))
-platform1.moving = False
-platform1.point = False
-
-# Collection of all sprites on screen
-all_sprites = pygame.sprite.Group()
-all_sprites.add(spider)
-all_sprites.add(platform1)
-
-# Separate platform sprite group
-plats = pygame.sprite.Group()
-plats.add(platform1)
-
-# Initialize starting screen random platforms
-for x in range(random.randint(5, 6)):
-    pl = platforms.Platform()
-    close = True
-    while close:
+def set_start_plats(plat_image, plats, all_sprites):
+    platform1 = platforms.Platform()
+    platform1.surf = pygame.transform.scale(plat_image, (params.WIDTH, 20))
+    platform1.rect = platform1.surf.get_rect(center=(params.WIDTH / 2, params.HEIGHT - 10))
+    platform1.moving = False
+    platform1.point = False
+    all_sprites.add(platform1)
+    plats.add(platform1)
+    for x in range(random.randint(5, 6)):
         pl = platforms.Platform()
-        close = platforms.check(pl, plats)
-    plats.add(pl)
-    all_sprites.add(pl)
+        close = True
+        while close:
+            pl = platforms.Platform()
+            close = platforms.check(pl, plats)
+        plats.add(pl)
+        all_sprites.add(pl)
+
+
+def build_start(game):
+    set_background(game.displaysurface, game.background)
+    set_start_plats(game.plat_image, game.plats, game.all_sprites)
+
+
+def init_agent(game, spider, agent, batch_size):
+    pass
+
+def run():
+    pygame.init()
+    game = SpiderJumpGame()
+    build_start(game)
+    running = True
+    while running:
+        set_background(game.displaysurface, game.background)
+        game.spider.update(game.plats)
+        # Track player inputs
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False  # Exit the while loop
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    game.spider.jump(game.plats)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_SPACE:
+                    game.spider.release_jump(game.plats)
+        # Initiate game over once the player falls off the screen
+        if game.spider.rect.top > params.HEIGHT:
+            for sprite in game.all_sprites:
+                sprite.kill()
+                time.sleep(1)
+                game.displaysurface.fill(params.WHITE)
+                pygame.display.update()
+                time.sleep(1)
+                pygame.quit()
+                sys.exit()
+        # Screen moving (in y-axis) and kills bottom platforms
+        if game.spider.rect.top <= params.HEIGHT / 3:
+            game.spider.pos.y += abs(game.spider.vel.y)
+            for pl in game.plats:
+                pl.rect.y += abs(game.spider.vel.y)
+                if pl.rect.top > params.HEIGHT:
+                    pl.kill()
+        # Generate new random platforms as player moves up
+        platforms.plat_gen(game.plats, game.all_sprites)
+        # Set game font and display game score
+        game_font_type = pygame.font.SysFont("Verdana", 20)
+        game_score = game_font_type.render(str(game.spider.score), True, (123, 255, 0))
+        game.displaysurface.blit(game_score, (params.WIDTH / 2, 10))
+        # Loops through all sprites on screen
+        for entity in game.all_sprites:
+            entity.draw(game.displaysurface)
+            entity.move()
+        pygame.display.update()
+        game.FramePerSec.tick(params.FPS)
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == '__main__':
+    run()
 
 """
     (Pseudocode)
