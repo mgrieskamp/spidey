b242efb5966101abf2cede0656118be7044528c0
shaun <shaunlee8@gmail.com>
2022-09-12 09:38:37 -0700

    added separate class file for D3Q and testing

--- D3Q.py
+++ D3Q.py
@@ -9,10 +9,11 @@ import torch.nn.functional as F
 import torch.optim as optim
 import copy
 import params
+
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-class DeepQAgent(torch.nn.Module):
+class D3QAgent(torch.nn.Module):
     def __init__(self, params):
         super().__init__()
         self.reward = 0
@@ -36,18 +37,23 @@ class DeepQAgent(torch.nn.Module):
         self.adv_layer = nn.Linear(512, 4)
 
         # Updates
-        self.target = None  # bellman equation target
+        self.target = None  # Bellman equation target Q
         self.action = None  # Action taken
         self.q = torch.sum(torch.multiply(self.q_values, torch.nn.functional.one_hot(self.action, 4)), dim=1)
         self.loss = F.huber_loss(input=self.q, target=self.target, reduction='mean', delta=1.0)
         self.optimizer = optim.Adam(self.parameters(), weight_decay=0, lr=self.learning_rate)
         self.update = self.optimizer.minimize(self.loss)
 
-        # weights
+        # Weights
         if self.load_weights:
             self.model = self.load_state_dict(torch.load(self.weights))
             print("weights loaded")
 
+    """
+    Forward call through 4 convolution layers and final split linear layer for dueling.
+    Input: Grayscale normalized image tensor
+    Output: Tensor of q_values for each action
+    """
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
@@ -57,8 +63,88 @@ class DeepQAgent(torch.nn.Module):
         value = self.value_layer(conv_value)
         adv = self.adv_layer(conv_adv)
         adv_average = torch.mean(adv, dim=1, keepdim=True)
-        q_values = value + adv - adv_average # is this a 1x4 tensor?
+        q_values = value + adv - adv_average  # is this a 1x4 tensor???
         return q_values
 
-    def select_action(self):
-        pass
+    """
+    Does a forward call and returns the index of the action with the highest Q value given a
+    state. Meant to be used on the main CNN.
+    Input: State sequence of (self.seq_size) frames
+    Output: Index of best action in action list
+    """
+    def get_highest_q_action(self, state):
+        with torch.no_grad():
+            q_values = self.forward(state)
+            best_action_index = torch.argmax(q_values, dim=1)  # are the dimensions correct???
+        return best_action_index.item()
+
+    """
+    Does a forward call and returns the Q value of an action at a specified index given a state
+    and an index. Meant to be used on the target CNN.
+    Input: State sequence of (self.seq_size) frames, Index of specified action
+    Output: Q value of specified action
+    """
+    def get_q_value_of_action(self, state, action_index):
+        with torch.no_grad():
+            q_values = self.forward(state)
+            q_value = torch.flatten(q_values)[action_index]
+        return q_value.item()
+
+
+class Memory(object):
+    def __init__(self, size=1000000, frame_h=200, frame_w=200, batch_size=32, seq_size=4):
+        self.counter = 0
+        self.current = 0
+        self.size = size
+        self.frame_h = frame_h
+        self.frame_w = frame_w
+        self.batch_size = batch_size
+        self.seq_size = seq_size
+
+        self.frames = np.empty((self.size, self.frame_h, self.frame_w), dtype=np.uint8)
+        self.actions = np.empty(self.size, dtype=np.int32)
+        self.rewards = np.empty(self.size, dtype=np.float32)
+        self.terminals = np.empty(self.size, dtype=np.bool)
+
+        self.states = np.empty((self.batch_size, self.seq_size,
+                                self.frame_h, self.frame_w), dtype=np.uint8)
+        self.new_states = np.empty((self.batch_size, self.seq_size,
+                                    self.frame_h, self.frame_w), dtype=np.uint8)
+        self.indices = np.empty(self.batch_size, dtype=np.int32)
+
+    def add_memory(self, frame, action, reward, is_terminal):
+        self.frames[self.current, ...] = frame
+        self.actions[self.current] = action
+        self.rewards[self.current] = reward
+        self.terminals[self.current] = is_terminal
+        self.counter = max(self.counter, self.current + 1)
+        self.current = (self.current + 1) % self.size
+
+    def get_state(self, index):
+        if self.counter == 0 or index < self.seq_size - 1:
+            pass
+        else:
+            return self.frames[index - self.seq_size + 1: index + 1, ...]
+
+    def get_indices(self):
+        for i in range(self.batch_size):
+            while True:
+                index = random.randint(self.seq_size, self.counter - 1)
+                if index < self.seq_size:
+                    continue
+                if index >= self.current >= index - self.seq_size:
+                    continue
+                if self.terminals[index - self.seq_size:index].any():
+                    continue
+                break
+            self.indices[i] = index
+
+    def get_minibatch(self):
+        if self.counter < self.seq_size:
+            pass
+        self.get_indices()
+        for i, idx in enumerate(self.indices):
+            self.states[i] = self.get_state(idx - 1)
+            self.new_states[i] = self.get_state(idx)
+        # unknown dimensions???
+        return self.states, self.actions, self.rewards, self.new_states, self.terminals
---
+++ D3Q_test_env.py
@@ -0,0 +1,145 @@
+import pygame
+import D3Q
+import player
+import platforms
+import params
+import sys
+import spritesheet
+import time
+import itertools
+from deepQ import DeepQAgent
+from random import randint
+import random
+import Q_params
+import statistics
+import torch.optim as optim
+import torch
+import torchvision
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+class SpiderJumpGame:
+    def __init__(self):
+        pygame.display.set_caption("SpiderJump")
+        self.displaysurface = pygame.display.set_mode((params.WIDTH, params.HEIGHT))
+        self.spider = player.Player()
+        self.all_sprites = pygame.sprite.Group()
+        self.all_sprites.add(self.spider)
+        self.plats = pygame.sprite.Group()
+        self.play_plats = pygame.sprite.Group()
+        self.plat_image = pygame.image.load('wood_platform.png')
+        self.plat_image.set_colorkey((0, 0, 0))
+        self.background = pygame.image.load("background.png")
+        self.FramePerSec = pygame.time.Clock()
+        self.font_type = pygame.font.SysFont("Verdana", 20)
+        self.game_over = False
+
+
+def set_background(displaysurface, background):
+    displaysurface.fill(params.WHITE)
+    bg_width, bg_height = background.get_width(), background.get_height()
+    for x, y in itertools.product(range(0, params.WIDTH, bg_width), range(0, params.HEIGHT, bg_height)):
+        displaysurface.blit(background, (x, y))
+
+
+def set_start_plats(plat_image, plats, all_sprites, play_plats):
+    platform1 = platforms.Platform()
+    platform1.surf = pygame.transform.scale(plat_image, (params.WIDTH, 20))
+    platform1.rect = platform1.surf.get_rect(center=(params.WIDTH / 2, params.HEIGHT - 10))
+    platform1.moving = False
+    platform1.point = False
+    all_sprites.add(platform1)
+    plats.add(platform1)
+    for x in range(random.randint(5, 6)):
+        pl = platforms.Platform()
+        close = True
+        while close:
+            pl = platforms.Platform()
+            close = platforms.check(pl, plats)
+        play_plats.add(pl)
+        plats.add(pl)
+        all_sprites.add(pl)
+
+
+def build_start(game):
+    set_background(game.displaysurface, game.background)
+    set_start_plats(game.plat_image, game.plats, game.all_sprites, game.play_plats)
+
+
+# 4 possible action states: left, right, jump, release jump
+def do_action(game, action):
+    if action[0] == 1 or action[1] == 1:
+        game.spider.agent_move(action)
+    elif action[2] == 1:
+        game.spider.jump(game.plats)
+    else:
+        game.spider.release_jump(game.plats)
+
+
+"""
+Obtains an RGB image of the current game screen, then grayscales and downsizes it to a 200 x 200
+np uint8 array for memory efficiency.
+Input: game object
+Output: grayscale np array (dtype = uint8) of current game frame
+"""
+def to_grayscale(game):
+    screen_area = pygame.Rect(0, params.HEIGHT, params.WIDTH, params.HEIGHT)
+    sub_surface = game.displaysurface.subsurface(screen_area)
+    RGB_data = pygame.surfarray.array3d(sub_surface)
+    tensor_RGB = torch.from_numpy(RGB_data)
+    grayscale_tensor = torchvision.transforms.functional.rgb_to_grayscale(tensor_RGB, 1)
+    resized_gray = torchvision.transforms.functional.resized_crop(grayscale_tensor, top=400, left=0, height=400,
+                                                                  width=400, size=(200, 200))
+    # normalized_gray = torch.div(resized_gray, 255)
+    np_gray = resized_gray.detach().cpu().numpy()
+    np_efficient = np_gray.astype(dtype=np.uint8)
+    return np_efficient
+
+
+"""
+Initializes the first sequence: Updates the agent.input field by cloning the first frame of
+the game into a (seq-size)-layer 3-dim tensor to be used for the first forward.
+"""
+def init_sequence(game, agent, seq_size):
+    first_frame = to_grayscale(game)
+    sequence_frames = np.repeat(first_frame, seq_size, axis=2)
+    sequence_tensor = torch.from_numpy(sequence_frames)
+    agent.input = torch.div(sequence_tensor, 255)
+
+
+# ??? maybe unnecessary
+def update_sequence(game, agent):
+    curr_frame = torch.from_numpy(to_grayscale(game))
+    norm_frame = torch.div(curr_frame, 255)
+    curr_sequence = agent.input
+    new_sequence = torch.stack((curr_sequence[:, :, 1:], norm_frame), dim=2)
+    agent.input = new_sequence
+
+
+"""
+Performs minibatch sampling from replay memory, sets the Bellman target Q for each step, and
+performs gradient descent every step.
+"""
+def train_short(memory, main_network, target_network):
+    states, actions, rewards, new_states, terminals = memory.get_minibatch()
+    for i in range(memory.batch_size):  # TODO: an array is more efficient than a for loop?
+        argmax_q_main = main_network.get_highest_q_action(new_states[i])
+        double_q = target_network.get_q_value_of_action(new_states[i], argmax_q_main)
+        main_network.target = rewards[i] + main_network.gamma * double_q * (1 - int(terminals[i]))
+        # Gradient descent: implement loss and optimizer
+        # loss = huber loss
+        # optimizer zero grad
+        # loss backward
+        # optimizer step
+
+
+# Reference https://github.com/gouxiangchen/dueling-DQN-pytorch/blob/master/dueling_dqn.py
+def training():
+    pygame.init()
+    q_params = Q_params.params_Q
+
+    main_network = D3Q.D3QAgent(q_params)
+    target_network = D3Q.D3QAgent(q_params)
